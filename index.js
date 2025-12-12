5ba8e932df0ea9f6a87ef599e7050390d55f4d96
Elaina Wittmer <elaina.n.wittmer@gmail.com>
2025-12-12 15:59:54 -0600

    Style overhaul

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from 'dotenv';
+import axios from "axios"
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -17,30 +20,25 @@ const db = new pg.Client({
 });
 db.connect();
 
-
 let sort = "newest";
 let remove = false;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-async function confirmPassword() {
-    let password = prompt("Please enter the password","");
-};
-
 async function getReadBooks(){
     let result;
     if (sort === "newest") {
-        result = await db.query("SELECT * FROM books WHERE date_read IS NOT NULL ORDER BY date_read DESC;");
+        result = await db.query("SELECT * FROM books ORDER BY id DESC;");
     }
     else if (sort === "oldest") {
-        result = await db.query("SELECT * FROM books WHERE date_read IS NOT NULL ORDER BY date_read ASC;");
+        result = await db.query("SELECT * FROM books ORDER BY id ASC;");
     }
     else if (sort === "title") {
         result = await db.query("SELECT * FROM books ORDER BY title ASC;");
     }
     else if (sort === "liked") {
-        result = await db.query("SELECT * FROM books WHERE liked = 'true';");
+        result = await db.query("SELECT * FROM books WHERE liked = 'true' ORDER BY id DESC;");
     }
     else if (sort === "disliked") {
         result = await db.query("SELECT * FROM books WHERE liked = 'false';");
@@ -51,17 +49,7 @@ async function getReadBooks(){
 app.get("/", async (req, res) => {
     try {
         const books = await getReadBooks();
-        let urls = [];
-        for (let i = 0; i < books.rows.length; i++) {
-            let url = "https://covers.openlibrary.org/b/isbn/"+ books.rows[i].isbn +"-S.jpg?default=false";
-            try {
-                let result = await axios.get(url);
-                urls.push(url);
-            } catch (err) {
-                urls.push("0");
-            }
-        }
-        res.render("index.ejs", {data: books.rows, urls: urls});
+        res.render("index.ejs", {data: books.rows});
     }
     catch (err){
         console.log(err.code);
@@ -69,79 +57,134 @@ app.get("/", async (req, res) => {
     }
 }); 
 
-app.get("/pair/:base/:target", async (req, res) => {
+app.get("/liked", async (req, res) => {
     try {
-        const base = req.params.base;
-        const target = req.params.target;
-        const url = baseURL + APIkey + "/pair/" + base + "/" + target;
-        const result = await axios.get(url);
-        res.render("index.ejs", {baseCode: result.data.base_code, targetCode: result.data.target_code, conversionRate: result.data.conversion_rate});
+        sort = "liked";
+        res.redirect("/");
     }
-    catch (error) {
-        res.status(500);
+    catch (err){
+        console.log(err.code);
+        res.redirect("/");
     }
+}); 
 
+app.get("/newest", async (req, res) => {
+    try {
+        sort = "newest";
+        res.redirect("/");
+    }
+    catch (err){
+        console.log(err.code);
+        res.redirect("/");
+    }
+}); 
+
+app.get("/oldest", async (req, res) => {
+    try {
+        sort = "oldest";
+        res.redirect("/");
+    }
+    catch (err){
+        console.log(err.code);
+        res.redirect("/");
+    }
+}); 
+
+
+app.get("/view", async (req, res) => {
+    try {
+        const searchId = req.query.id;
+        const idResults = await db.query("SELECT * FROM books WHERE id = "+ searchId + ";");
+        res.render("view.ejs", {data: idResults.rows[0]});
+    }
+    catch (err){
+        console.log(err.code);
+        res.redirect("/");
+    }
+}); 
+
+app.get("/edit", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM books WHERE id = " + [req.query.id] + ";");
+        res.render("edit.ejs", {data: result.rows[0]});
+    }
+    catch (err){
+        console.log(err.code);
+        res.redirect("/");
+    }
 });
+
+
+// app.get("/pair/:base/:target", async (req, res) => {
+//     try {
+//         const base = req.params.base;
+//         const target = req.params.target;
+//         const url = baseURL + APIkey + "/pair/" + base + "/" + target;
+//         const result = await axios.get(url);
+//         res.render("index.ejs", {baseCode: result.data.base_code, targetCode: result.data.target_code, conversionRate: result.data.conversion_rate});
+//     }
+//     catch (error) {
+//         res.status(500);
+//     }
+
+// });
 
 app.get("/submit", async (req, res) => {
-    try {
-        console.log(req.query);
-        const base = req.query.base;
-        const target = req.query.target;
-        const amount = parseFloat(req.query.amount);
-        console.log(amount);
-        if (isNaN(amount)) {
-            console.log("Please enter a numerical amount.");
-            res.render("index.ejs", {error: "error"});
-        }
-        else{
-            const url = baseURL + APIkey + "/pair/" + base + "/" + target; 
-            const result = await axios.get(url);
-            res.render("index.ejs", {baseCode: result.data.base_code, targetCode: result.data.target_code, conversionRate: result.data.conversion_rate, baseAmount: Math.floor(amount * 100) / 100});
-        }
-
-    }
-    catch (error) {
-        res.status(500);
-    }
-
-});
-
-app.get("/submit/:id", async (req, res) => {
-    console.log("Editing...");
+    console.log("Starting...")
     try {
         let liked;
+        let date = req.query.date;
         if (req.query.liked == null) {
             liked = false;
         } else {
             liked = true;
         };
-        await db.query("UPDATE books SET title = $1, author = $2, liked = $3, date_read = $4, isbn = $5, notes = $6 WHERE id = $7", [req.query.title, req.query.name, liked, req.query.date, req.query.isbn, req.query.notes, req.params.id]);
+        if (req.query.date === "") {
+            date = null;
+        };
+        try {
+            await db.query("INSERT INTO books(title, author, liked, date_read, isbn, notes) VALUES ($1, $2, $3, $4, $5, $6)", [req.query.title, req.query.name, liked, date, req.query.isbn, req.query.notes]);
+        } catch (err) {
+            console.log(err);
+            alert("Error entering book data. Redirecting...")
+        }
+        console.log("Redirecting...");
+        res.redirect("/");
     } catch (err) {
         console.log(err);
-        console.log("Error updating book data.");
     }
-    res.redirect("/");
 });
 
-app.get("/delete/:id", async (req, res) => {
-    try {
-        await db.query("DELETE FROM books WHERE id = $1", [req.params.id]);
-    } catch (err) {
-        console.log(err);
-        console.log("Error updating book data.");
-    }
-    res.redirect("/");
-});
+// app.get("/submit/:id", async (req, res) => {
+//     console.log("Editing...");
+//     try {
+//         let liked;
+//         if (req.query.liked == null) {
+//             liked = false;
+//         } else {
+//             liked = true;
+//         };
+//         await db.query("UPDATE books SET title = $1, author = $2, liked = $3, date_read = $4, isbn = $5, notes = $6 WHERE id = $7", [req.query.title, req.query.name, liked, req.query.date, req.query.isbn, req.query.notes, req.params.id]);
+//     } catch (err) {
+//         console.log(err);
+//         console.log("Error updating book data.");
+//     }
+//     res.redirect("/");
+// });
+
+// app.get("/delete/:id", async (req, res) => {
+//     try {
+//         await db.query("DELETE FROM books WHERE id = $1", [req.params.id]);
+//     } catch (err) {
+//         console.log(err);
+//         console.log("Error updating book data.");
+//     }
+//     res.redirect("/");
+// });
 
 app.get("/add", async (req, res) => {
     res.render("add.ejs");
 });
-
-app.get("/edit/:id", async (req, res) => {
-    const result = await db.query("SELECT * FROM books WHERE id = $1", [req.params.id]);
-    res.render("edit.ejs", {data: result.rows});
-})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
